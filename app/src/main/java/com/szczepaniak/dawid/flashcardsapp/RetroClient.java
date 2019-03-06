fb05d27e1f3bae6646c405abb43d6b7be370f416
HardMobileGames <hardmobilegames@gmail.com>
2019-03-06 16:41:14 +0100

    User login 2.

--- app/src/main/java/com/szczepaniak/dawid/flashcardsapp/RetroClient.java
+++ app/src/main/java/com/szczepaniak/dawid/flashcardsapp/RetroClient.java
@@ -1,19 +1,28 @@
 package com.szczepaniak.dawid.flashcardsapp;
 
-import retrofit2.Call;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
 
 public class RetroClient {
 
 
-    private static final String ROOT_URL = "http://192.168.0.123:8080/";
+    private static final String ROOT_URL = "http://192.168.0.182:8080/";
 
     private static Retrofit getRetrofitInstance() {
+
+        OkHttpClient.Builder okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(60 * 5, TimeUnit.SECONDS)
+                .readTimeout(60 * 5, TimeUnit.SECONDS)
+                .writeTimeout(60 * 5, TimeUnit.SECONDS);
+        okHttpClient.interceptors().add(new AddCookiesInterceptor());
+        okHttpClient.interceptors().add(new ReceivedCookiesInterceptor());
+
         return new Retrofit.Builder()
                 .baseUrl(ROOT_URL)
+                .client(okHttpClient.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
@@ -22,6 +31,4 @@ public class RetroClient {
     public static ApiService getApiService() {
         return getRetrofitInstance().create(ApiService.class);
     }
-
-
 }
